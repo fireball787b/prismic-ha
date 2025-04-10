2a2c8057f0d917f275e1945821b450531ee1b9bb
fireball787b <fireball787b@gmail.com>
2025-04-10 15:50:25 +0200

    fix

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,8 +4,7 @@ import React from "react"; // ✅ required for createElement
 // ✅ Mock next/image to behave like a regular <img>
 jest.mock("next/image", () => ({
     __esModule: true,
-    default: (props: any) => {
-        const { src, alt, ...rest } = props;
-        return React.createElement("img", { src, alt, ...rest });
+    default: ({ priority, ...props }: any) => {
+        return React.createElement("img", props); // strip `priority`
     },
 }));
